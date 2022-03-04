ca1749e22715a04363a896ad47024818a15520ab
camillapalombi <camilla.palombi14@gmail.com>
2022-03-04 15:57:36 +0100

    Struttura JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//stampare in html 5 numeri casuali (math random tra due numeri in un ciclo settato a 5 ripetizioni)
+//settiamo un timer di 30s con setTimeout
+//allo scadere del setTimeout facciamo in modo che i 5 numeri scompaiano
+//crea 5 prompt nei quali chiedi all'utente di iserire i numeri che ha visto in ordire numerico (uno in ogni prompt)
+//l'utente inserisce i numeri nei prompt
+//scrivo in html quanti e quali numeri ha azzeccato l'utente nei prompt
+
+//VARIABILI GLOBALI
+
+let printRandomNumbers = document.querySelector('.random-numbers'); //stampa dei Nrandom
+
+let printFinalResult = document.querySelector('.result');//stampa del risultato finale
